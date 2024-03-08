b8f026d162b5ede19c4331ecb5bb92bfd5662ad0
jlbuild <juliabuildbot@gmail.com>
2024-03-08 00:51:55 +0000

    acl_jll build 2.3.2+0

--- src/wrappers/armv7l-linux-gnueabihf.jl
+++ src/wrappers/armv7l-linux-gnueabihf.jl
@@ -3,12 +3,18 @@ export chacl, getfacl, libacl, setfacl
 
 using Attr_jll
 JLLWrappers.@generate_wrapper_header("acl")
+JLLWrappers.@declare_library_product(libacl, "libacl.so.1")
 JLLWrappers.@declare_executable_product(chacl)
 JLLWrappers.@declare_executable_product(getfacl)
-JLLWrappers.@declare_library_product(libacl, "libacl.so.1")
 JLLWrappers.@declare_executable_product(setfacl)
 function __init__()
     JLLWrappers.@generate_init_header(Attr_jll)
+    JLLWrappers.@init_library_product(
+        libacl,
+        "lib/libacl.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
     JLLWrappers.@init_executable_product(
         chacl,
         "bin/chacl",
@@ -17,12 +23,6 @@ function __init__()
     JLLWrappers.@init_executable_product(
         getfacl,
         "bin/getfacl",
-    )
-
-    JLLWrappers.@init_library_product(
-        libacl,
-        "lib/libacl.so",
-        RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_executable_product(
