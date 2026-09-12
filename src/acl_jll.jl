b2e4c660cbe21c7c6d0dacdd7b5e51858cb4b7e1
jlbuild <juliabuildbot@gmail.com>
2026-09-12 14:56:56 +0000

    acl_jll build 2.4.0+0

--- src/acl_jll.jl
+++ src/acl_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("acl")
-JLLWrappers.@generate_main_file("acl", UUID("ed5aba05-e74d-5cf7-8b09-107ba3463b8e"))
+JLLWrappers.@generate_main_file("acl", Base.UUID("ed5aba05-e74d-5cf7-8b09-107ba3463b8e"))
 end  # module acl_jll
